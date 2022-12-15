b80d234112507cad8a8a86041250ae5cbf331a57
Rohit Nisal <rohitnisal@gmail.com>
2022-12-14 18:55:47 -0800

    - New Project added into git

---
+++ AutomaticLib/AutomaticLib/AutomaticLibApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutomaticLibApp.swift
+//  AutomaticLib
+//
+//  Created by Rohit Nisal on 12/14/22.
+//
+
+import SwiftUI
+
+@main
+struct AutomaticLibApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AutomaticLib/AutomaticLib/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AutomaticLib
+//
+//  Created by Rohit Nisal on 12/14/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
